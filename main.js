ec2e054fbb7e04b42128d95099b5889dcff954df
Effencee <107486971+Effencee@users.noreply.github.com>
2022-06-14 17:12:39 +0200

    adding files

---
+++ main.js
@@ -0,0 +1,35 @@
+const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
+const txt = ['tekst1', 'tekst2', 'tekst3']
+
+let text = 0;
+let letter = -10;
+
+// Implementacja
+const addLetter = () => {
+    // Użyj w środku setTimeout
+    if (letter >= 0) {
+        spnText.textContent += txt[text][letter];
+    }
+    letter++;  
+            if (letter == txt[text].length) {
+                text++;
+                if (text == txt.length) return;
+
+                return setTimeout(() => {
+                    letter = -10;
+                    spnText.textContent = '';
+                    addLetter();
+                }, 2000)
+            }
+        
+    setTimeout(addLetter, 100);
+}
+addLetter(); //pierwsze wywołanie
+
+
+// Animacja kursora (zostaw bez zmian)
+const cursorAnimation = () => {
+    spnCursor.classList.toggle('active');
+}
+setInterval(cursorAnimation, 400);
